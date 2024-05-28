cac9d68ed7ecf8091d6a0525304c02f51b08ca2b
raine <rainey888@mail.ru>
2024-05-28 10:04:24 +0700

    - Change mapper as extension-fun

--- app/src/main/java/com/raineyi/imagesearchapp/data/repository/ImageRepositoryImpl.kt
+++ app/src/main/java/com/raineyi/imagesearchapp/data/repository/ImageRepositoryImpl.kt
@@ -1,23 +1,31 @@
 package com.raineyi.imagesearchapp.data.repository
 
 import android.util.Log
-import com.raineyi.imagesearchapp.data.mapper.ImageMapper
 import com.raineyi.imagesearchapp.data.network.ApiService
+import com.raineyi.imagesearchapp.data.network.model.ImageDto
 import com.raineyi.imagesearchapp.domain.Image
 import com.raineyi.imagesearchapp.domain.ImageRepository
 import javax.inject.Inject
 
 class ImageRepositoryImpl @Inject constructor(
-    private val mapper: ImageMapper,
     private val apiService: ApiService
 ) : ImageRepository {
     override suspend fun loadImages(request: String, page: Int): List<Image>? {
         return try {
             val imageResponseDto = apiService.getImageResponse(request = request, page = page)
-            imageResponseDto.imageList?.map { mapper.mapDtoToImage(it) }
+            imageResponseDto.imageList?.map { it.toImage() }
         } catch (e: Exception) {
             Log.d("TEST_API", e.message.toString())
             null
         }
+    }
+
+    private fun ImageDto.toImage(): Image {
+        return Image(
+            id = this.id,
+            title = this.title,
+            imageUrl = this.imageUrl,
+            link = this.link
+        )
     }
 }
